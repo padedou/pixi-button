c30093a58a6269a6c9e28251732994f779e163b1
padedou <padedou@gmail.com>
2018-03-24 16:23:49 +0200

    Initial commit

---
+++ Button.js
@@ -0,0 +1,34 @@
+class Button extends PIXI.Sprite {
+    constructor({
+        x = 0,
+        y = 0,
+        color = 0xffffff,
+        alpha = 1,
+        txtValue = 'button',
+        txtStyle = {},
+        interactive = true,
+        shape = {
+            type: 'rect',
+            options: {
+                width: 100,
+                height: 50
+            }
+        }
+    } = {}) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.alpha = alpha;
+        this.txtValue = txtValue;
+        this.txtStyle = txtStyle;
+        this.interactive = interactive;
+        this.shape = shape;
+
+        this.generate();
+    }
+
+    generate() {
+        
+    }
+}
